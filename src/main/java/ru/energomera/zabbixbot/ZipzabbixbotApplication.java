aa8faa2b151e038639639b2c972422c0aa696b22
Konstantin Boryagin <nokss89@gmail.com>
2021-11-27 10:34:36 +0300

    initialize

---
+++ src/main/java/ru/energomera/zabbixbot/ZipzabbixbotApplication.java
@@ -0,0 +1,13 @@
+package ru.energomera.zabbixbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipzabbixbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipzabbixbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/energomera/zabbixbot/ZipzabbixbotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.energomera.zabbixbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipzabbixbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
