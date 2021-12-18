efe4495c98d855d408740b5564983beea646ed27
Konstantin Boryagin <nokss89@gmail.com>
2021-12-18 16:28:45 +0300

    heroku deploy1

--- src/main/java/ru/energomera/zabbixbot/controller/HerokuMonitoring.java
+++ src/main/java/ru/energomera/zabbixbot/controller/HerokuMonitoring.java
@@ -3,7 +3,6 @@ package ru.energomera.zabbixbot.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
